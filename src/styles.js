98fa83941404842eb0f58ef48a2bbf62a6c842f8
Salman Zafar <maanizfar@gmail.com>
2020-07-26 18:09:17 +0500

    add material ui

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffcc33",
+      dark: "#ffb347",
+    },
+    secondary: {
+      main: "#fefefe",
+    },
+  },
+});
