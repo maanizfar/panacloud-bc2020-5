369bd1037a48db51b5d2a5d224a65ded6aac56d3
Salman Zafar <maanizfar@gmail.com>
2020-07-27 17:49:49 +0500

    animate button

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,37 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 const useStyles = makeStyles((theme) => ({
   btn: {
     padding: "0.75rem 2rem",
-    border: "1px solid grey",
+    border: "2px solid grey",
     borderRadius: "50px",
-    width: 180,
 
     "&:hover": {
-      "& div": {
-        display: "flex",
-      },
+      borderColor: theme.palette.green,
     },
   },
 
   iconContainer: {
-    display: "none",
+    display: "flex",
+    width: 0,
     marginLeft: theme.spacing(1),
+    transform: "scale(0)",
   },
 }));
 
 const MyButton = ({ text }) => {
   const classes = useStyles();
+  const ref = useRef(null);
+
+  const timing = {
+    duration: 500,
+    iterations: 1,
+    fill: "forwards",
+    easing: "ease-in",
+  };
+
+  const play = () => {
+    console.log("play");
+    ref.current.animate(
+      [
+        { transform: "scale(0)", width: 0, marginRight: 0 },
+        { transform: "scale(1)", width: 20, marginRight: "16px" },
+      ],
+      timing
+    );
+  };
+
+  const reverse = () => {
+    console.log("reverse");
+    ref.current.animate(
+      [
+        { transform: "scale(1)", width: 20, marginRight: "16px" },
+        { transform: "scale(0)", width: 0, marginRight: 0 },
+      ],
+      timing
+    );
+  };
 
   return (
-    <Button variant="h1" className={classes.btn}>
+    <Button
+      className={classes.btn}
+      onMouseEnter={() => {
+        play();
+      }}
+      onMouseLeave={() => reverse()}
+    >
       <span>{text}</span>
-      <div className={classes.iconContainer}>
+      <span ref={ref} className={classes.iconContainer}>
         <ArrowRightAltIcon />
-      </div>
+      </span>
     </Button>
   );
 };
